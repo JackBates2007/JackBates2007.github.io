74bae950e72bb5ec682905d08c1e640f59690644
Jack Bates <500213024@fvtc.edu>
2026-01-28 12:07:46 -0600

    Added welcome div

---
+++ site.js
@@ -0,0 +1,28 @@
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+const WelcomeMessage = document.querySelector("#Welcome")
+
+if (isMorning) {
+
+    WelcomeMessage.textContent = "It is the morning"
+
+}
+
+if (isAfternoon) {
+
+    WelcomeMessage.textContent = "It is the afternoon"
+
+}
+
+if (isEvening) {
+
+    WelcomeMessage.textContent = "It is the evening"
+
+}
+
+
+console.log(WelcomeMessage)
