b8cbeab4a89ad8dae4e24290da204c3ada8f1db6
Jack Bates <500213024@fvtc.edu>
2026-02-17 21:23:53 -0600

    Added Carousel

--- site.js
+++ site.js
@@ -1,3 +1,4 @@
+//Time Added
 const hours = new Date().getHours() // get the current hour
 
 const isMorning = hours >= 4 && hours < 12 // is it morning?
@@ -24,7 +25,47 @@ if (isEvening) {
 
 }
 
-
+//Secret Message
 console.log(WelcomeMessage)
 
 localStorage.setItem("It's a secret to everybody.", "+100 Rupees")
+
+//Carousel
+const urls = [
+    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+].map(url => { (new Image()).src = url; return url })
+
+const images = document.querySelectorAll('#carousel img')
+
+const next = document.querySelector('#next')
+const prev = document.querySelector('#prev')
+
+let currentImage = 0
+const showImages = () => {
+    const offset = currentImage % urls.length
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length
+        image.src = urls[imageIndex]
+    })
+}
+
+showImages()
+
+setInterval(() => {
+    currentImage = currentImage + 1
+    showImages()
+}, 5000)
+
+next.addEventListener('click', () => {
+    currentImage = currentImage + 1
+    showImages()
+})
+
+prev.addEventListener('click', () => {
+    currentImage = currentImage - 1
+    showImages()
+})
